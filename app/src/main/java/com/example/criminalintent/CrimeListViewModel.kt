8cbc90277e6d7676e088a9889b2684a22543d2c7
WerdnaLes <werdnales@gmail.com>
2022-10-14 14:27:35 +0300

    Added a RecycleView. Chap 10(Displaying Lists with Recycler View)

---
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.criminalintent
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+class CrimeListViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
+
+    val crimes = mutableListOf<Crime>()
+
+    init {
+        for (i in 0 until 100) {
+            val crime = Crime(
+                id = UUID.randomUUID(),
+                title = "Crime #$i",
+                date = Date(),
+                isSolved = i % 2 == 0
+            )
+
+            crimes += crime
+        }
+    }
+}
