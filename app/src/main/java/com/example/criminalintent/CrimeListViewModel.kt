40c730f33d38dc5c5b74385c033453e458de2691
WerdnaLes <werdnales@gmail.com>
2022-10-15 18:50:00 +0300

    Temp

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.criminalintent
 
+import android.text.format.DateFormat
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class CrimeListViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
@@ -13,11 +16,20 @@ class CrimeListViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
             val crime = Crime(
                 id = UUID.randomUUID(),
                 title = "Crime #$i",
-                date = Date(),
+                date = FormattedDate,
                 isSolved = i % 2 == 0
             )
 
             crimes += crime
         }
+    }
+}
+
+val FormattedDate = object : Date() {
+    override fun toString(): String {
+        var formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        val date = LocalDate.parse( formatter).toString()
+
+        return date
     }
 }
